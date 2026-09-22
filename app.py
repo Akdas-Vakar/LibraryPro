557eed139ff96936cf4a23d991accbc09b658b91
Akdas-Vakar <akdasvakar98765@gmail.com>
2026-09-23 00:19:24 +0530

    Fix imports to work with standard Render gunicorn app:app command

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import os
 import sys
-
-
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
-
-from app import app
+from backend.app import app
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
